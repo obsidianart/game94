89e60201db20fbc66f99390a75b37926f9b3a63e
stefano solinas <obsidianart@gmail.com>
2016-10-29 22:59:47 +0100

    add multiple levels

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -4,13 +4,17 @@ import { Level } from './level'
 @Injectable()
 export class GameService {
 	levels = {
-		level0: <Level>{
+		fruits: <Level>{
 			title: 'Fruits',
 			answers: ['banana', 'apple']
 		},
-		level1: <Level>{
+		vegetables: <Level>{
 			title: 'Vegetables',
 			answers: ['cucumber', 'leek']
+		},
+		birds: <Level>{
+			title: 'Birds',
+			answers: ['pidgeon', 'robin']
 		}
 	}
 
@@ -18,13 +22,13 @@ export class GameService {
 
 	getLevel():Promise<Level> {
 		return new Promise(resolve=>{
-			setTimeout(()=>resolve(this.levels.level0), 1000)
+			setTimeout(()=>resolve(this.levels.fruits), 1000)
 		})
 	}
 
 	isAnswerForLevel(_answer:string):Promise<Boolean> {
 		return new Promise((resolve,reject)=>{
-			let answer = this.levels.level0.answers.find(ans=>ans.toLowerCase()==_answer.toLowerCase())
+			let answer = this.levels.fruits.answers.find(ans=>ans.toLowerCase()==_answer.toLowerCase())
 			answer?resolve(answer):reject(_answer)
 		})
 	}
